c0a360ffccf78fe5be541e7206f21aae3f00f062
Rony Langbord <ronylangbords@gmail.com>
2017-02-15 15:57:57 +0200

    Update middleware for localhost

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const PORT = process.env.PORT || 3000;
 //create our app
 var app = express();
 
-
 app.use(function(req, res, next) {
-  if (req.headers['x-forwarded-proto'] === 'http') {
-    next();
-  } else {
+  if (req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
+
+  } else {
+    next();
   }
 });
 
